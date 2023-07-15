62b0b1e051a31d48bc700623effd4c8c3b0a5d7e
Simon Erlic <simon@erlic.ca>
2023-07-15 16:36:20 -0700

    Initial Commit

---
+++ MagicOrb/MagicOrbApp.swift
@@ -0,0 +1,20 @@
+//
+//  MagicOrbApp.swift
+//  MagicOrb
+//
+//  Created by Simon Erlic on 2023-07-15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MagicOrbApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
